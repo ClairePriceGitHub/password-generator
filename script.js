ccb8f950c48ee75f42a5ee4e7f86cb4462eb35ba
Claire Price <claire.price@mail.com>
2023-12-18 00:44:14 +0000

    add prompt typeof test for input values

--- script.js
+++ script.js
@@ -92,71 +92,28 @@ var upperCasedCharacters = [
 // --------------------------------------------------------------------
 // Function to prompt user for password options
 function getPasswordOptions() {
-  // var promptQuestions = [
-  //   prompt("How many characters does your password have?")];
-  // var promptAnswers = [0];
-  
-  // // For prompt to check input values and replace in promptAnswers array
-  // var numCharInput = 0;
-  // for (var i=0; i < promptQuestions.length; i++) {
-  //   // if (promptQuestions[i] < 8 || promptQuestions[i] > 128) {
-  //   //   alert("Please enter a number between 8 and 128");
-  //   //   getPasswordOptions();
-  //   //   break;
-  //   // } else {
-  //     numCharInput += promptQuestions[i];
-  //     promptAnswers.splice(i, 1, numCharInput);
-  //   //}
-  // }
-
-
-
-
-
   var promptAnswers = [];
-  var numCharInput = 0;
+  // Function to test inputed prompt values and push to array
   var promptTestInput = function() {
-    var promptQuestions = prompt("How many characters does your password have?");
+    var promptQuestions = prompt("How many characters would you like your password to have?");
     if (promptQuestions < 8 || promptQuestions > 128) {
-      alert("Please enter a number between 8 and 128");
-      promptTest();
+      alert("Error:   Please enter a number between 8 and 128");
+      promptTestInput();
+    } else if (isNaN(promptQuestions * 1)) {
+      alert("Error:   Please enter a numeric value (e.g. 12)");
+      promptTestInput();
     } else {
-      numCharInput += promptQuestions;
-      promptAnswers.push(numCharInput);
+      promptAnswers.push(promptQuestions);
     }
   }
   promptTestInput();
-
-
-
-  // if (promptQuestions < 8 || promptQuestions > 128) {
-  //   alert("Please enter a number between 8 and 128");
-  //   prompt("How many characters does your password have?");
-  // } 
-  // numCharInput += promptQuestions;
-  // promptAnswers.push(numCharInput);
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   var confirmQuestions = [
     confirm("does your password have special characters?"),
     confirm("does your password have numeric characters?"),
     confirm("does your password have uppercase characters?")];
   var confirmAnswers = [false, false, false];
-  // For confirm questions: to change false to true following input
+  // For confirm questions to change false to true as per input
   for (var i=0; i < confirmQuestions.length; i++) {
     for (var j=0; j < confirmAnswers.length; j++) {
     if (confirmQuestions[i] == true) {
@@ -164,9 +121,10 @@ function getPasswordOptions() {
       }
     }
   }
-  // One array with all answers
+
+  // Return one array with all answers
   var inputAnswers = promptAnswers.concat(confirmAnswers);
-  return(inputAnswers);
+  return inputAnswers;
 }
 
 // --------------------------------------------------------------------
@@ -182,61 +140,49 @@ function getRandom(arr) {
 function generatePassword() {
   var output = getPasswordOptions();
   // Create array with confirm outputs only
-  var promptOutput = output.slice(1);
+  var confirmOutput = output.slice(1);
   // Convert password length prompt from string to number
-  var confirmOutput = (output[0]) * 1;
+  var promptOutput = (output[0]) * 1;
   var arraySelection = [];
   var password = [];
   // Use input to combine which arrays are to be randomly selected from
   // password.push to ensure at least one character from each array used
-  if (promptOutput[0] === true) {
+  if (confirmOutput[0] === true) {
     arraySelection.push(specialCharacters);
     password.push(getRandom(specialCharacters));
   }  
-  if (promptOutput[1] === true) {
+  if (confirmOutput[1] === true) {
     arraySelection.push(numericCharacters);
     password.push(getRandom(numericCharacters));
   }
-  if (promptOutput[2] === true) {
+  if (confirmOutput[2] === true) {
     arraySelection.push(upperCasedCharacters);
     password.push(getRandom(upperCasedCharacters));
   }
   arraySelection.push(lowerCasedCharacters);
   password.push(getRandom(lowerCasedCharacters));
 
-  // Combine selected arrays into one array rather than nested
+  // Combine selected arrays into one array rather than nested, to then do random selection
   var flatArraySelection = arraySelection.flat();
 
   var confirmOutputLength = 0;
   if (arraySelection.length === 4) {
-    confirmOutputLength = confirmOutput - 4;
+    confirmOutputLength = promptOutput - 4;
   } else if (arraySelection.length === 3) {
-    confirmOutputLength = confirmOutput - 3;
+    confirmOutputLength = promptOutput - 3;
   } else if (arraySelection.length === 2) {
-    confirmOutputLength = confirmOutput - 2;
+    confirmOutputLength = promptOutput - 2;
   } else {
-    confirmOutputLength = confirmOutput - 1;
+    confirmOutputLength = promptOutput - 1;
   }
 
   for (var i=0; i < confirmOutputLength; i++) {
     password.push(getRandom(flatArraySelection));
   }
 
+  // Password array to string without commas
   return password.toString().replace(/,/g,"");
-
-  
-
- 
-
 }
-
-
- 
-
-
-
-
-
 
 // --------------------------------------------------------------------
 // Get references to the #generate element
